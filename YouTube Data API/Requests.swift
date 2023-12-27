509d0f611d61c7563a785b450d9968b235551204
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-27 19:06:21 +0100

    Successful playlist items fetch from API

--- YouTube Data API/Requests.swift
+++ YouTube Data API/Requests.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+func fetchPlaylistItems() {
+    
+    // Very important: GET Requests have no body to the request, instead everything goes on the URL
+    
+    // Create URL Components
+    guard var urlComponents = URLComponents(string: YOUTUBE_API + GET_PLAYLIST_ITEMS_ENDPOINT) else {
+        return
+    }
+    
+    // Add parameters as query items
+    urlComponents.queryItems = [
+        URLQueryItem(name: "part", value: "snippet"),
+        URLQueryItem(name: "playlistId", value: PLAYLIST_ID),
+        URLQueryItem(name: "maxResults", value: "10"),
+        URLQueryItem(name: "key", value: API_KEY)
+    ]
+    
+    // Create URL
+    guard let url = urlComponents.url else {
+        return
+    }
+    
+    // Create URL Request
+    var urlRequest = URLRequest(url: url)
+            
+    // Specify HTTP Request Method
+    urlRequest.httpMethod = "GET"
+    
+    
+    let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: {
+        (data, response, error) in
+        
+        if error != nil {
+            print("Error occured.")
+            return
+        }
+        
+        if let response = response as? HTTPURLResponse {
+            print("Response Status code: \(response.statusCode)\n")
+        }
+        
+        if let data = data {
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            }
+            catch {
+                return
+            }
+        }
+    })
+    task.resume()
+}

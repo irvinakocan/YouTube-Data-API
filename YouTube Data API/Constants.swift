9af4cb4163e297f9e65080d7e01dcc49b33ae9f1
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-12-30 00:37:33 +0100

    POST Request
    Implemented inserting YouTube video into specified playlist.

--- YouTube Data API/Constants.swift
+++ YouTube Data API/Constants.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-let API_KEY = "AIzaSyCCBpm4zk40B8Luss2bVTahshCIDtNR3PQ"
+let API_KEY = "AIzaSyC5eq_KuRHWCWM3qfvkhS0VjA26XHa3wI8"
 let YOUTUBE_API = "https://www.googleapis.com/youtube/v3"
 let GET_PLAYLIST_ITEMS_ENDPOINT = "/playlistItems"
-let PLAYLIST_ID = "PLxhXZ_bcn1VoNU1VCcbxlZGOKHjqoFwSw"
+let POST_PLAYLIST_ITEMS_ENDPOINT = "/playlistItems"
+let MY_PLAYLIST_ID = "PLxhXZ_bcn1VoNU1VCcbxlZGOKHjqoFwSw"
+
+let POST_CHANEL_ID = "UCJeH7gl6PbDVV4DTldIOPOA"
+let POST_PLAYLIST_ID = "PL198741246C1CF574"
+let POST_VIDEO_ID = "OjUiVJUC4tY"
+
+let MY_ACCESS_TOKEN = "ya29.a0AfB_byAj4aP-Aj-DCUcxb6T-TPGuviFU99tC7yaIGNbODz0dPvfBgO_GB9DPc5Twv_P6iyYQ8iK1Y5aXwneFJC-Nei-6YJNAE5JYqJrE5jYmKUeOk4M9VxeG5i9RRjpuerJsmSKlYZtW7DtiU4M_TUpUnXxkkRaeaAebaCgYKAZESARASFQHGX2MiiEE-CCMiarhNu_zy9C2qxw0171"
+
--- YouTube Data API/Requests.swift
+++ YouTube Data API/Requests.swift
@@ -19,7 +19,7 @@ func fetchPlaylistItems() {
     // Add parameters as query items
     urlComponents.queryItems = [
         URLQueryItem(name: "part", value: "snippet"),
-        URLQueryItem(name: "playlistId", value: PLAYLIST_ID),
+        URLQueryItem(name: "playlistId", value: MY_PLAYLIST_ID),
         URLQueryItem(name: "maxResults", value: "10"),
         URLQueryItem(name: "key", value: API_KEY)
     ]
@@ -41,6 +41,7 @@ func fetchPlaylistItems() {
             print("Response Status code: \(response.statusCode)\n")
         }
         
+        // Converting json data into eye-readable data
         if let data = data {
             do {
                 let json = try JSONSerialization.jsonObject(with: data, options: [])
@@ -49,6 +50,72 @@ func fetchPlaylistItems() {
             catch {
                 return
             }
+        }
+    })
+    task.resume()
+}
+
+func addItemIntoPlaylist() {
+    
+    // Create URL Components
+    guard var urlComponents = URLComponents(string: YOUTUBE_API + POST_PLAYLIST_ITEMS_ENDPOINT) else {
+        return
+    }
+    
+    // Add query parameters
+    urlComponents.queryItems = [
+        URLQueryItem(name: "key", value: API_KEY),
+        URLQueryItem(name: "part", value: "snippet")
+    ]
+    
+    // Create URL
+    guard let url = urlComponents.url else {
+        return
+    }
+    
+    // Create URL Request
+    var urlRequest = URLRequest(url: url)
+    
+    // Specify HTTP Request Method
+    urlRequest.httpMethod = "POST"
+    
+    // Set HTTP Request Headers
+    urlRequest.setValue("Bearer " + MY_ACCESS_TOKEN, forHTTPHeaderField: "Authorization")
+    urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+    urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    // Specify Request Body
+    let requestBody: [String: Any] = [
+        "snippet": [
+            "playlistId": MY_PLAYLIST_ID,
+            "resourceId": [
+                "channelId": POST_CHANEL_ID,
+                "kind": "youtube#video",
+                "playlistId": POST_PLAYLIST_ID,
+                "videoId": POST_VIDEO_ID
+            ]
+        ]
+    ]
+    
+    // Turn request body parameters into json and set them
+    guard let httpBody = try? JSONSerialization.data(withJSONObject: requestBody) else {
+        return
+    }
+    urlRequest.httpBody = httpBody
+    
+    let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: {
+        (data, response, error) in
+        
+        guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+        }
+        
+        guard let responseJSON = try? JSONSerialization.jsonObject(with: data) else {
+            return
+        }
+        if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
         }
     })
     task.resume()
